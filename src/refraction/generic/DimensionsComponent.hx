27e0fb213dc9636c00bdd6402adbf5606cf75697
Oliver Xu <oxu.wex@gmail.com>
2017-12-11 03:18:33 -0500

    allow overriding name for dimension

--- src/refraction/generic/DimensionsComponent.hx
+++ src/refraction/generic/DimensionsComponent.hx
@@ -11,9 +11,9 @@ class DimensionsComponent extends Component
 	public var width:Int;
 	public var height:Int;
 	
-	public function new(_width:Int = 20, _height:Int = 20) 
+	public function new(_width:Int = 20, _height:Int = 20, _name_overrides = "dim_comp") 
 	{
-		super("dim_comp");
+		super(_name_overrides);
 		width = _width;
 		height = _height;
 	}
